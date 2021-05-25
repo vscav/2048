5860347a98c1e3e1b16e4907fea7db4eb2193fb0
vscav <vscavinner@gmail.com>
2021-05-25 03:04:45 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { join } from 'path'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
+  base: process.env.NODE_ENV === 'production' ? '/2048/' : '/',
   alias: {
     '/@/': join(__dirname, 'src'),
   },
