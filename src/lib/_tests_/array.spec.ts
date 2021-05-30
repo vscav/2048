374d414dc9ba5d03673f2dfe0b6fa9df80a26901
vscav <vscavinner@gmail.com>
2021-05-30 23:59:04 +0200

    Array intern library tests

---
+++ src/lib/_tests_/array.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai'
+
+import { fil, filterObjs } from '../array'
+
+describe('Array tests', () => {
+  it('checking array filter', () => {
+    expect(fil((e) => e < 5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to.deep.eq([
+      1,
+      2,
+      3,
+      4,
+    ])
+    expect(fil((e) => e === 'foo', [])).to.deep.eq([])
+  })
+  const obj: { [key: string]: number }[] = [{ n: 15 }, { n: 2, m: 15 }]
+  it('checking object filter', () => {
+    expect(
+      filterObjs((o: { [key: string]: number }) => o.n === 15)(obj),
+    ).to.deep.eq([{ n: 15 }])
+  })
+})
